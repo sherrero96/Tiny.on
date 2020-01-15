6c75e8208aef688e2b69a317403b7102702f68b7
Guillerm097 <guillermopgdc@gmail.com>
2020-01-15 15:10:03 +0100

    Hazelcast erased

--- src/main/java/urlshortener/config/HazelcastCacheConfig.java
+++ src/main/java/urlshortener/config/HazelcastCacheConfig.java
@@ -1,21 +1,21 @@
-package urlshortener.config;
+// package urlshortener.config;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.hazelcast.config.Config;
+// import com.hazelcast.config.EvictionPolicy;
+// import com.hazelcast.config.MapConfig;
+// import com.hazelcast.config.MaxSizeConfig;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class HazelcastCacheConfig {
+// @Configuration
+// public class HazelcastCacheConfig {
 
-    @Bean
-    public Config hazelcastConfig(){
-       return new Config().setInstanceName("hazelcast-instance")
-                .addMapConfig(new MapConfig().setName("qr")
-                .setMaxSizeConfig(new MaxSizeConfig(500,MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                .setEvictionPolicy(EvictionPolicy.LRU)
-                .setTimeToLiveSeconds(50000)); // Infinite(0)
-    }
-}
+//     @Bean
+//     public Config hazelcastConfig(){
+//        return new Config().setInstanceName("hazelcast-instance")
+//                 .addMapConfig(new MapConfig().setName("qr")
+//                 .setMaxSizeConfig(new MaxSizeConfig(500,MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+//                 .setEvictionPolicy(EvictionPolicy.LRU)
+//                 .setTimeToLiveSeconds(50000)); // Infinite(0)
+//     }
+// }
