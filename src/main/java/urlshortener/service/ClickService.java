ee68ef523e4b44a727eb90df589e5fae2e8b28bc
sherrero96 <sergio.herrero.barco@mail.com>
2020-01-15 01:05:00 +0100

    More

--- src/main/java/urlshortener/service/ClickService.java
+++ src/main/java/urlshortener/service/ClickService.java
@@ -3,6 +3,7 @@ package urlshortener.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import urlshortener.domain.Click;
@@ -24,11 +25,31 @@ public class ClickService {
         this.clickRepository = clickRepository;
     }
 
-    @CacheEvict(key = "#hash", value = "lastStats")
     public void saveClick(String hash, String ip, String country, String platform, Date date) {
         Click cl = ClickBuilder.newInstance().hash(hash).createdNow().ip(ip).country(country).platform(platform).created(date).build();
         cl = clickRepository.save(cl);
         log.info(cl != null ? "[" + hash + "] saved with id [" + cl.getId() + "]" : "[" + hash + "] was not saved");
+        updateCacheLastStats(hash);
+    }
+
+    @CachePut(value = "lastStats", key = "#hash")
+    public ArrayList<String> updateCacheLastStats(String hash){
+        List<Click> hashes = clickRepository.findByHash(hash);
+        ArrayList<String> result = new ArrayList<>();
+        result.add(0, String.valueOf(hashes.size()));
+        if(hashes.size() == 0){
+            // Ninguna visita realizada... ponemos datos fijados
+            result.add(1, "Desconocido");
+            result.add(2, "Desconocido");
+            result.add(3, "Desconocido");
+        }else{
+            Click lastClick = hashes.get(hashes.size() - 1);
+            result.add(1, lastClick.getCreated().toString());
+            result.add(2, lastClick.getCountry());
+            result.add(3, lastClick.getPlatform());
+        }
+
+        return result;
     }
 
     /**
